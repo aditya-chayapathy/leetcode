f092bf8a71dfff156e47012a7a870ad61014fbfd
Aditya C <aditya.chayapathy@gmail.com>
2018-06-11 06:44:09 -0700

    Adding solution

---
+++ Solutions/Hamming_Distance.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        bin_x = "{0:b}".format(x)
+        bin_y = "{0:b}".format(y)
+        if len(bin_x) != len(bin_y):
+            smaller = ""
+            if len(bin_x) > len(bin_y):
+                smaller = "y"
+            else:
+                smaller = "x"
+            if smaller == "x":
+                diff = len(bin_y) - len(bin_x)
+                temp = diff * '0'
+                bin_x = temp + bin_x
+            else:
+                diff = len(bin_x) - len(bin_y)
+                temp = diff * '0'
+                bin_y = temp + bin_y
+
+        count = 0
+        for i in range(len(bin_x)):
+            if bin_x[i] != bin_y[i]:
+                count += 1
+
+        return count
