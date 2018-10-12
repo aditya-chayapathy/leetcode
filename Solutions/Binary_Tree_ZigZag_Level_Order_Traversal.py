502ea974578379e7f9e276ea9af7218cfd042295
Aditya C <aditya.chayapathy@gmail.com>
2018-10-12 07:35:45 -0700

    Adding solution

---
+++ Solutions/Binary_Tree_ZigZag_Level_Order_Traversal.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def __init__(self):
+        self.traversal = []
+
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        self.recurrsiveSolution(root, 0)
+
+        result = []
+        for i in range(1, len(self.traversal), 2):
+            self.traversal[i].reverse()
+
+        return self.traversal
+
+    def recurrsiveSolution(self, root, level):
+        if root is None:
+            return
+
+        if len(self.traversal) <= level:
+            self.traversal.append([])
+
+        self.traversal[level].append(root.val)
+        self.recurrsiveSolution(root.left, level + 1)
+        self.recurrsiveSolution(root.right, level + 1)
