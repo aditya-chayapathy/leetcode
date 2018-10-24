e07192a4913c273e2e10f66d5afbd5aede1ac7fd
Aditya C <aditya.chayapathy@gmail.com>
2018-10-24 08:48:25 -0700

    Adding solution

--- Solutions/Add_Two_Numbers.py
+++ Solutions/Add_Two_Numbers.py
@@ -14,13 +14,12 @@ class Solution:
         cur_carry = 0
         res = ListNode(None)
         temp = res
-        count = 0
-        while (l1 != None and l2 != None):
+        while l1 is not None and l2 is not None:
             prev_carry = cur_carry
             cur_carry = 0
 
             cur_sum = l1.val + l2.val + prev_carry
-            if (cur_sum >= 10):
+            if cur_sum >= 10:
                 cur_carry = 1
                 cur_sum -= 10
 
@@ -28,16 +27,15 @@ class Solution:
             temp.next = ListNode(None)
             temp = temp.next
 
-            count += 1
             l1 = l1.next
             l2 = l2.next
 
-        while (l1 != None):
+        while l1 is not None:
             prev_carry = cur_carry
             cur_carry = 0
 
             cur_sum = l1.val + prev_carry
-            if (cur_sum >= 10):
+            if cur_sum >= 10:
                 cur_carry = 1
                 cur_sum -= 10
 
@@ -45,15 +43,14 @@ class Solution:
             temp.next = ListNode(None)
             temp = temp.next
 
-            count += 1
             l1 = l1.next
 
-        while (l2 != None):
+        while l2 is not None:
             prev_carry = cur_carry
             cur_carry = 0
 
             cur_sum = l2.val + prev_carry
-            if (cur_sum >= 10):
+            if cur_sum >= 10:
                 cur_carry = 1
                 cur_sum -= 10
 
@@ -61,17 +58,16 @@ class Solution:
             temp.next = ListNode(None)
             temp = temp.next
 
-            count += 1
             l2 = l2.next
 
-        if (l1 == None and l2 == None and cur_carry == 1):
+        if l1 is None and l2 is None and cur_carry == 1:
             temp.val = 1
             temp.next = None
 
             return res
 
         temp = res
-        while (temp.next.val != None):
+        while temp.next.val is not None:
             temp = temp.next
         temp.next = None
 
